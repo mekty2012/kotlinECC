582d9ab61447f29f8615d5edbc689ecd9a361200
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2018-11-30 13:22:24 +0900

    Update ECCPoint.kt

--- ECCPoint.kt
+++ ECCPoint.kt
@@ -1,5 +1,3 @@
-package adunkrul.ECC
-
 import java.math.BigInteger
 
 data class ECCPoint(val x : BigInteger?, val y : BigInteger?, val curve : ECCCurve) {
@@ -13,22 +11,50 @@ data class ECCPoint(val x : BigInteger?, val y : BigInteger?, val curve : ECCCur
     }
     else {
       isO = false
-    }
-    val left = y.modPow(2, curve.Prime)
-    val right = x.modPow(3, curve.Prime).add(curve.a.multiply(x)).mod(curve.Prime).add(curve.b).mod(curve.Prime)
-    if (!(left.subtract(right)).mod(curve.Prime).equals(BigInteger.ZERO)) {
-      throw ECCException("Point is not on curve.")
+      val left = y!!.modPow(BigInteger.valueOf(2L), curve.Prime)
+      val right = x!!.modPow(BigInteger.valueOf(3L), curve.Prime).add(curve.a.multiply(x!!)).mod(curve.Prime).add(curve.b).mod(curve.Prime)
+      if (!(left.subtract(right).mod(curve.Prime).equals(BigInteger.ZERO))) {
+        throw ECCException("Point is not on curve.\nCurve:y^2=x^3+${curve.a}x+${curve.b} (mod ${curve.Prime}\nPoint:(${this.x}, ${this.y})")
+      }
     }
   }
-  fun discreteDivision(dividend : BigInteger, divisor:BigInteger) : BigInteger {
-    var n = BigInteger.ONE
-    var div = if (dividend.compareTo(BigInteger.ZERO)<0) (dividend.mod(curve.Prime)).add(curve.Prime) else if (dividend.compareTo(curve.Prime)>=0) dividend.mod(curve.Prime) else dividend
-    var sum = if (divisor.compareTo(BigInteger.ZERO)<0) (divisor.mod(curve.Prime)).add(curve.Prime) else if (divisor.compareTo(curve.Prime)>=0) divisor.mod(curve.Prime) else divisor
-    while (!sum.equals(dividend)) {
-      sum = (sum.add(divisor)).mod(curve.Prime)
-      n = n.add(BigInteger.ONE)
+  private fun discreteDivision(dividend : BigInteger, divisor:BigInteger) : BigInteger {
+    // returns dividend / divisor (mod curve.Prime)
+    val inv = divisor.modPow(curve.Prime.subtract(BigInteger.valueOf(2L)), curve.Prime)
+    return inv.multiply(dividend)
+  }
+  fun addPointCoordinate(other:ECCPoint) : Pair<BigInteger?, BigInteger?> {
+    if (curve != other.curve) {
+      throw ECCException("Point on different curve.")
     }
-    return n
+    if (isO) {
+      return Pair(other.x, other.y)
+    }
+    else if (other.isO) {
+      return Pair(x, y)
+    }
+    else {
+      if (x!!.equals(other.x!!)) {
+        if (y!!.equals(other.y!!.negate())) {
+          return Pair(null, null)
+        }
+        else if (y!!.mod(curve.Prime).equals(BigInteger.ZERO)) {
+          return Pair(null, null)
+        }
+        else {
+          val lambda = discreteDivision(x!!.modPow(BigInteger.valueOf(2L), curve.Prime).multiply(BigInteger.valueOf(3L)).add(curve.a).mod(curve.Prime), y!!.multiply(BigInteger.valueOf(2L)).mod(curve.Prime))
+          val ansx = lambda.modPow(BigInteger.valueOf(2L), curve.Prime).subtract(x!!.multiply(BigInteger.valueOf(2L)).mod(curve.Prime)).mod(curve.Prime)
+          val ansy = lambda.multiply(x!!.subtract(ansx).mod(curve.Prime)).subtract(y!!).mod(curve.Prime)
+          return Pair(ansx, ansy)
+        }
+      }
+      else {
+        val lambda = discreteDivision(other.y!!.subtract(y!!), other.x!!.subtract(x!!))
+        val ansx = lambda.modPow(BigInteger.valueOf(2L), curve.Prime).subtract(x!!).subtract(other.x!!).add(curve.Prime.multiply(BigInteger.valueOf(2L))).mod(curve.Prime)
+        val ansy = lambda.multiply(x!!.subtract(ansx).mod(curve.Prime)).subtract(y!!).mod(curve.Prime)
+        return Pair(ansx, ansy)
+      }
+    }
   }
   fun addPoint(other : ECCPoint) : ECCPoint {
     if (curve != other.curve) {
@@ -41,39 +67,24 @@ data class ECCPoint(val x : BigInteger?, val y : BigInteger?, val curve : ECCCur
       return this
     }
     else {
-      if (x.equals(other.x)) {
-        if (y.equals(other.y)) {
-          val lambda = discreteDivision(x.modPow(2, curve.Prime).multiply(BigInteger("3").add(curve.a).mod(curve.Prime)), y.multiply(BigInteger("2")).mod(curve.Priem))
-          val ansx = lambda.modPow(2, curve.Prime).subtract(x.multiply(BigInteger("2")).mod(curve.Prime)).add(curve.Prime).mod(curve.Prime)
-          val ansy = lambda.multiply(x.subtract(ansx).add(curve.Prime).mod(curve.Prime)).mod(curve.Prime).subtract(y).add(curve.Prime).mod(curve.Prime)
-          return ECCPoint(ansx, ansy, curve)
-        }
-        else {
-          return ECCPoint(null, null, curve)
-        }
-      }
-      else {
-        val lambda = discreteDivision(other.y.subtract(y), other.x.subtract(x))
-        val ansx = lambda.modPow(2, curve.Prime).subtract(x).subtract(other.x).add(curve.Prime.multiply(BigInteger("2"))).mod(curve.Prime)
-        val ansy = lambda.multiply(x.subtract(ansx).add(curve.Prime).mod(curve.Prime)).subtract(y).add(curve.Prime).mod(curve.Prime)
-        return ECCPoint(ansx, ansy, curve)
-      }
+      val coordinate = this.addPointCoordinate(other)
+      return ECCPoint(coordinate.first, coordinate.second, this.curve)
     }
   }
   fun scalarMultiple(n:BigInteger):ECCPoint {
     val bit = n.toString(2)
     var ans = this
     for (i in 1 until bit.length) {
+      if (bit.get(i) == '1') {
         ans = ans.addPoint(ans).addPoint(this)
-        if (bit.get(i) == '1') {
       }
       else {
-        ans = ans.add(ans)
+        ans = ans.addPoint(ans)
       }
     }
     return ans
   }
   fun negatePoint():ECCPoint {
-    return ECCPoint(this.x, this.y.negate(), this.curve)
+    return ECCPoint(this.x, this.y?.negate(), this.curve)
   }
 }
