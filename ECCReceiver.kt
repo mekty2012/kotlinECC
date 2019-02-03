d3f78b1575aa78332255d97fc954365c067ee14b
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2019-02-04 03:40:08 +0900

    Update ECCReceiver.kt

--- ECCReceiver.kt
+++ ECCReceiver.kt
@@ -1,52 +1,43 @@
 import java.math.BigInteger
-import java.util.Random
 
 fun main(args:Array<String>) {
-  val random = Random()
-  var ans : List<String>
-  var curveCount = 0
-  readCurve@while (true) {
-    try {
-      val curveSocket : java.io.File = java.io.File(curveSocketName)
-      ans = curveSocket.readLines()
-      break@readCurve
-    } catch (e : java.io.FileNotFoundException) {
-      Thread.sleep(10)
-      println("curve Try:${curveCount++}")
-      continue@readCurve
+  
+  val encodingBitLength = args[1].toInt()
+  val encodingCurveSocket = java.io.File(encodingCurveSocketName)
+  val encodingAlicePointSocket = java.io.File(encodingAlicePointSocketName)
+  val encodingBobPointSocket = java.io.File(encodingBobPointSocketName)
+  
+  val encodingCurveNameList = encodingCurveSocket.readLines()
+  val encodingAlicePointNameList = encodingAlicePointSocket.readLines()
+  val encodingBobPointNameList = encodingBobPointSocket.readLines()
+  
+  val sharedSecrets = Array<BigInteger>(encodingCurveNameList.size) {BigInteger.ZERO}
+  
+  val threads = Array(encodingCurveNameList.size) {
+    Thread {
+      val result = sharedECCPointReceiver(encodingCurveNameList[it], encodingAlicePointNameList[it], encodingBobPointNameList[it], encodingBitLength, java.security.SecureRandom())
+      sharedSecrets[it] = result.x ?: BigInteger.ZERO
     }
   }
-  val curveParam : List<BigInteger> = deserializeCurve(ans)
-  println("Prime:${curveParam[0]}\na:${curveParam[1]}\nb:${curveParam[2]}\nGx:${curveParam[3]}\nGy:${curveParam[4]}")
-  val curve : ECCCurve = ECCCurve(curveParam[0], curveParam[1], curveParam[2], curveParam[3], curveParam[4])
-  var privateKey : BigInteger
-  while (true) {
-    privateKey = BigInteger(curve.Prime.bitLength(), random)
-    println("privateKey try : ${privateKey}")
-    if (privateKey.compareTo(curve.Prime) < 0) {
-      break
-    }
+  for (thread in threads) {
+    thread.start()
   }
+  for (thread in threads) {
+    thread.join()
+  }
+  
+  Thread.sleep(1000) // need to check whether encoding ended or not
+  decode(cipherText, sharedSecrets, encodingBitLength, decodedText)
+  
+  val macBitLength = args[2].toInt()
+  
+  val macCurveSocket = java.io.File(macCurveSocketName)
+  val macAlicePointSocket = java.io.File(macAlicePointSocketName)
+  val macBobPointSocket = java.io.File(macBobPointSocketName)
+  
+  val macCurveNameList = macCurveSocket.readLines()
+  val macAlicePointNameList = macAlicePointSocket.readLines()
+  val macBobPointNameList = macBobPointSocket.readLines()
 
-  val bobPoint : ECCPoint = curve.G.scalarMultiple(privateKey)
-  val bobPointSocket : java.io.File = java.io.File(bobPointSocketName)
-  bobPointSocket.appendText("isO:${if (bobPoint.isO) 0 else 1}\nx:${bobPoint.x}\ny:${bobPoint.y}")
-  println("isO:${if (bobPoint.isO) 0 else 1}\nx:${bobPoint.x}\ny:${bobPoint.y}")
-  var alicePoint : ECCPoint
-  var aliceCount = 0
-  readalice@while (true) {
-    try {
-      val alicePointSocket : java.io.File = java.io.File(alicePointSocketName)
-      val alicePointData : List<BigInteger?> = deserializePoint(alicePointSocket.readLines())
-      alicePoint = ECCPoint(alicePointData[1], alicePointData[2], curve)
-      break@readalice
-    } catch (e : java.io.FileNotFoundException) {
-      Thread.sleep(10)
-      println("alice try : ${aliceCount++}")
-      continue@readalice
-    }
-  }
-  val resultPoint = alicePoint.scalarMultiple(privateKey)
-  val sharedSecret = bobPoint.x
-  println(sharedSecret)
+  
 }
