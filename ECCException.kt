dd0a4f543db58c19c8745f26fad08e0ce4fa7407
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2019-02-04 03:38:14 +0900

    Update ECCException.kt

--- ECCException.kt
+++ ECCException.kt
@@ -1,3 +1,4 @@
-class ECCException(override var message:String): Exception(message) {
+class ECCException(override var message:String): Exception(message)
 
-}
+class ECCIllegalArgumentException(override var message:String): IllegalArgumentException(message)
+
