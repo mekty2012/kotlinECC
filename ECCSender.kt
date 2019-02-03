af3f68166b23131eeb0bbbd082974aa568e83d5c
mekty2012 <41375658+mekty2012@users.noreply.github.com>
2019-02-04 03:40:32 +0900

    Update ECCSender.kt

--- ECCSender.kt
+++ ECCSender.kt
@@ -1,42 +1,71 @@
 import java.math.BigInteger
-import java.util.Random
 
 fun main(args:Array<String>) {
-  val random = Random()
-  val bitLength : Int = readLine()!!.toInt()
-  val curve : ECCCurve = getECCCurve(bitLength)
-  val curveSocket : java.io.File = java.io.File(curveSocketName)
-  println("Prime:${curve.Prime}\na:${curve.a}\nb:${curve.b}\nGx:${curve.Gx}\nGy:${curve.Gy}")
-  curveSocket.appendText("Prime:${curve.Prime}\na:${curve.a}\nb:${curve.b}\nGx:${curve.Gx}\nGy:${curve.Gy}")
-  var privateKey : BigInteger
-  while (true) {
-    privateKey = BigInteger(bitLength, random)
-    println("privateKey try : ${privateKey}")
-    if (privateKey.compareTo(curve.Prime) < 0) {
-      break
+  val random = java.security.SecureRandom()
+  val plainFile = java.io.File(plainText)
+  val fileSize = plainFile.length()
+  val encodingBitLength = args[1].toInt()
+  
+  val encodingCurveSocket = java.io.File(encodingCurveSocketName)
+  val encodingAlicePointSocket = java.io.File(encodingAlicePointSocketName)
+  val encodingBobPointSocket = java.io.File(encodingBobPointSocketName)
+  
+  val encodingCurveNameList = mutableListOf<String>()
+  val encodingAlicePointNameList = mutableListOf<String>()
+  val encodingBobPointNameList = mutableListOf<String>()
+  for (i in 0 .. (fileSize / encodingBitLength)) {
+    val curveRandomString = getRandomString(20, random)
+    encodingCurveSocket.appendText(curveRandomString + "\n")
+    encodingCurveNameList.add(curveRandomString)
+    val alicePointRandomString = getRandomString(20, random)
+    encodingAlicePointSocket.appendText(alicePointRandomString + "\n")
+    encodingAlicePointNameList.add(alicePointRandomString)
+    val bobPointRandomString = getRandomString(20, random)
+    encodingBobPointSocket.appendText(bobPointRandomString + "\n")
+    encodingBobPointNameList.add(bobPointRandomString)
+  }
+  
+  val sharedSecrets = Array<BigInteger>(encodingCurveNameList.size) {BigInteger.ZERO}
+  val threads = Array(encodingCurveNameList.size) {
+    Thread {
+      val result = sharedECCPointSender(encodingCurveNameList[it], encodingAlicePointNameList[it], encodingBobPointNameList[it], encodingBitLength, java.security.SecureRandom())
+      sharedSecrets[it] = result.x ?: BigInteger.ZERO
     }
   }
-  val alicePoint : ECCPoint = curve.G.scalarMultiple(privateKey)
-  val alicePointSocket : java.io.File = java.io.File(alicePointSocketName)
-  alicePointSocket.appendText("isO:${if (alicePoint.isO) 0 else 1}\nx:${alicePoint.x}\ny:${alicePoint.y}")
-  println("isO:${if (alicePoint.isO) 0 else 1}\nx:${alicePoint.x}\ny:${alicePoint.y}")
-  var bobPoint : ECCPoint
-  var bobCount = 0
-  read@while (true) {
-    try {
-      val bobPointSocket : java.io.File = java.io.File(bobPointSocketName)
-      val bobPointData : List<BigInteger?> = deserializePoint(bobPointSocket.readLines())
-      bobPoint = ECCPoint(bobPointData[1], bobPointData[2], curve)
-      break@read
-    } catch (e : java.io.FileNotFoundException) {
-      Thread.sleep(10)
-      println("bob Try : ${bobCount++}")
-      continue@read
-    }
+  for (thread in threads) {
+    thread.start()
   }
-  val resultPoint = bobPoint.scalarMultiple(privateKey)
-  val sharedSecret = bobPoint.x
-  println(sharedSecret)
+  for (thread in threads) {
+    thread.join()
+  }
+  
+  encode(plainText, sharedSecrets, cipherText, encodingBitLength, random)
+  
+  val macBitLength = args[2].toInt()
+  
+  val macCurveSocket = java.io.File(macCurveSocketName)
+  val macAlicePointSocket = java.io.File(macAlicePointSocketName)
+  val macBobPointSocket = java.io.File(macBobPointSocketName)
+  
+  val macCurveNameList = mutableListOf<String>()
+  val macAlicePointNameList = mutableListOf<String>()
+  val macBobPointNameList = mutableListOf<String>()
+  for (i in 0 .. (fileSize / macBitLength)) {
+    val curveRandomString = getRandomString(20, random)
+    macCurveSocket.appendText(curveRandomString + "\n")
+    macCurveNameList.add(curveRandomString)
+    val alicePointRandomString = getRandomString(20, random)
+    macAlicePointSocket.appendText(alicePointRandomString + "\n")
+    macAlicePointNameList.add(alicePointRandomString)
+    val bobPointRandomString = getRandomString(20, random)
+    macBobPointSocket.appendText(bobPointRandomString + "\n")
+    macBobPointNameList.add(bobPointRandomString)
+  }
+  
+  
 }
 
+fun getRandomString(size:Int, random:java.security.SecureRandom):String {
+  return (Array(size) {'a' + random.nextInt(26)}).joinToString("")
+}
 
